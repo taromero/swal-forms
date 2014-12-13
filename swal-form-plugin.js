55d59fe4ef97668e954b35d987967d4bde524d82
Tomas Romero <canotto90@gmail.com>
2014-12-13 17:35:20 -0300

    Added comments to code and a README.md

--- swal-form-plugin.js
+++ swal-form-plugin.js
@@ -1,6 +1,10 @@
+// extend swal with a function for adding forms
 swal.withForm = function() {
+  // initialize with field values supplied on `swal.withForm` call
   var swalForm = new SwalForm(arguments[0].formFields)
+  // prevent successive calls to add duplicated form fields
   swalForm.removeSwalForm()
+  // make form values inserted by the user available on `doneFunction`
   swalForm.addWayToGetFormValuesInDoneFunction(arguments)
 
   // forward arguments
@@ -8,13 +12,17 @@ swal.withForm = function() {
 
   var htmlForm = swalForm.generateHtmlForm()
   swalForm.insertFormInSwalModal(htmlForm)
+  // by default swal modals have tabIndex=-1 which prevents
+  // text selection and user input on tags
   swalForm.makeInputsWritable()
 }
 
+// constructor for helper object
 function SwalForm(formFields) {
   this.formFields = formFields
 }
 
+// helper methods
 extend(SwalForm.prototype, {
   formClass: 'swalForm',
   generateHtmlForm: function() {
@@ -36,6 +44,7 @@ extend(SwalForm.prototype, {
     var self = this
     var doneFunction = swalArgs[1]
     swalArgs[1] = function() {
+      // make form values available at `this` variable inside doneFunction
       this.swalForm = self.getFormValues()
       doneFunction.apply(this, arguments)
     }
